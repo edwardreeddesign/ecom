4f356a84e6ea72a9e4b4be07f44970f40a3156bd
Ed Reed <edreed@Eds-MacBook-Air.local>
2022-07-02 08:10:41 -0400

    created order screen with details

--- models/Products.js
+++ models/Products.js
@@ -2,16 +2,16 @@ import mongoose from 'mongoose';
 
 const productSchema = new mongoose.Schema(
   {
-    name: { type: 'string', required: true },
-    slug: { type: 'string', required: true, unique: true },
-    category: { type: 'string', required: true },
-    image: { type: 'string', required: true },
+    name: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    category: { type: String, required: true },
+    image: { type: String, required: true },
     price: { type: Number, required: true },
-    brand: { type: 'string', required: true },
+    brand: { type: String, required: true },
     rating: { type: Number, required: true, default: 0 },
     numReviews: { type: Number, required: true, default: 0 },
     countInStock: { type: Number, required: true, default: 0 },
-    description: { type: 'string', required: true },
+    description: { type: String, required: true },
   },
   {
     timestamps: true,
@@ -20,5 +20,4 @@ const productSchema = new mongoose.Schema(
 
 const Product =
   mongoose.models.Product || mongoose.model('Product', productSchema);
-
 export default Product;
